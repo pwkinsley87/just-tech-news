be59158ea8ec11748923330d41c23d163bbe56b2
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-08-17 13:30:31 -0500

    restructuring again in attempt to correct 404 error

--- models/post.js
+++ models/post.js
@@ -16,7 +16,10 @@ class Post extends Model {
           'post_url',
           'title',
           'created_at',
-          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+          [
+            sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+            'vote_count'
+          ]
         ],
         include: [
           {
--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
-
-const hbs = exphbs.create({});
- 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(require('./controllers/'));
+// turn on routes
+app.use(routes);
 
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
