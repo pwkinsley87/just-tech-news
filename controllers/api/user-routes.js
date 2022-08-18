0d9d7c12fedc1847950c6d44db30b42135275c93
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-08-18 10:31:53 -0500

    added login and logout code

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -6,11 +6,15 @@ router.get('/', (req, res) => {
   User.findAll({
     attributes: { exclude: ['password'] }
   })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        res.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
 });
 
 router.get('/:id', (req, res) => {
@@ -88,7 +92,14 @@ router.post('/login', (req, res) => {
       return;
     }
 
-    res.json({ user: dbUserData, message: 'You are now logged in!' });
+    req.session.save(() => {
+      // declare session variables 
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
   });
 });
 
@@ -134,4 +145,14 @@ router.delete('/:id', (req, res) => {
     });
 });
 
-module.exports = router;
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -39,4 +39,17 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+router.get('/', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
 module.exports = router;
